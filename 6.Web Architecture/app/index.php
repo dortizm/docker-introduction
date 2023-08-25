030ec3af705a645fe1c731d182f93dcacd6b5165
dortiz <daniel.ortiz.maturana@gmail.com>
2023-08-25 08:05:56 -0400

    se agrega funcionamiento php en lampp

---
+++ 6.Web Architecture/app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Mi Página en Docker</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>¡Hola desde Docker!</h1>
+    <p>Este es un ejemplo de página PHP en un contenedor Docker.</p>
+    <?php
+                echo "<h1>¡Hola, OpenWebinars te da la bienvenida!</h1>";
+                ?>
+</body>
+</html>
